ab5030479c35fe3a34653c616f0f54f843622411
mrshu <mrshu@cisco.com>
2023-05-26 14:20:46 +0200

    update: Run sliger through black and isort
    * Ensure the Sliger code is black and isort compliant.
    
    Signed-off-by: mrshu <mrshu@cisco.com>

--- sliger/__init__.py
+++ sliger/__init__.py
@@ -5,7 +5,7 @@ import sys
 import time
 from pathlib import Path
 from pprint import pprint
-from typing import Optional, Callable
+from typing import Callable, Optional
 
 import toml
 import typer
@@ -13,7 +13,7 @@ from google.oauth2 import service_account
 from googleapiclient.discovery import build
 from googleapiclient.errors import HttpError
 from googleapiclient.http import MediaFileUpload
-from jinja2 import Environment, BaseLoader
+from jinja2 import BaseLoader, Environment
 
 app = typer.Typer()
 state = {}
@@ -22,7 +22,6 @@ state = {}
 # StringLoader is a custom Jinja2 template loader for rendering templates
 # provided as strings, instead of loading from files or other sources.
 class StringLoader(BaseLoader):
-
     # get_source() returns a tuple (template, None, lambda: True) for Jinja2
     # to load the template source, without a filename/path, and always up to date.
     def get_source(self, environment, template):
@@ -36,7 +35,7 @@ def load_jinja_environment(config_path: str) -> Environment:
 
         def import_function(function_name: str) -> Callable:
             if "." in function_name:
-                mod_name, func_name = function_name.rsplit('.', 1)
+                mod_name, func_name = function_name.rsplit(".", 1)
                 mod = importlib.import_module(mod_name)
                 res = getattr(mod, func_name)
             else:
@@ -44,11 +43,12 @@ def load_jinja_environment(config_path: str) -> Environment:
             return res
 
         function_map = config["function_map"]
-        function_map = {name: import_function(function_name) for name, function_name in
-                        function_map.items()}
+        function_map = {
+            name: import_function(function_name)
+            for name, function_name in function_map.items()
+        }
     else:
         function_map = {}
-
 
     env = Environment(loader=StringLoader())
     env.globals.update(function_map)
@@ -60,14 +60,12 @@ def load_jinja_environment(config_path: str) -> Environment:
 def duplicate_presentation(copy_title: str = typer.Option(...)):
     presentation_id = state["presentation_id"]
     try:
-
-        drive_service = build('drive', 'v3', credentials=state["creds"])
-        body = {
-            'name': copy_title
-        }
-        drive_response = drive_service.files().copy(
-            fileId=presentation_id, body=body).execute()
-        presentation_copy_id = drive_response.get('id')
+        drive_service = build("drive", "v3", credentials=state["creds"])
+        body = {"name": copy_title}
+        drive_response = (
+            drive_service.files().copy(fileId=presentation_id, body=body).execute()
+        )
+        presentation_copy_id = drive_response.get("id")
 
         ids = []
         file_id = presentation_copy_id
@@ -77,25 +75,28 @@ def duplicate_presentation(copy_title: str = typer.Option(...)):
                 # Handle error
                 print(exception)
             else:
-                print(f'Request_Id: {request_id}')
-                print(F'Permission Id: {response.get("id")}')
-                ids.append(response.get('id'))
+                print(f"Request_Id: {request_id}")
+                print(f'Permission Id: {response.get("id")}')
+                ids.append(response.get("id"))
 
         # pylint: disable=maybe-no-member
         batch = drive_service.new_batch_http_request(callback=callback)
         user_permission = {
-            'type': 'anyone',
-            'role': 'writer',
+            "type": "anyone",
+            "role": "writer",
         }
-        batch.add(drive_service.permissions().create(
-            fileId=file_id,
-            body=user_permission,
-            fields='id',
-        ))
+        batch.add(
+            drive_service.permissions().create(
+                fileId=file_id,
+                body=user_permission,
+                fields="id",
+            )
+        )
         batch.execute()
 
         print(
-            f"Created new presentation at https://docs.google.com/presentation/d/{presentation_copy_id}/edit")
+            f"Created new presentation at https://docs.google.com/presentation/d/{presentation_copy_id}/edit"
+        )
         return presentation_copy_id
 
     except HttpError as err:
@@ -143,8 +144,10 @@ def delete_slide(id: int = typer.Option(...)):
 
         slide_ids = [slide_id for slide_id in slides]
         if slide_to_delete_id is None and slide_to_delete_id not in slide_ids:
-            print(f"\nSlide number {id} doesn't exist (id: {slide_id})."
-                  f"Please input an existing slide from your presentation.")
+            print(
+                f"\nSlide number {id} doesn't exist (id: {slide_id})."
+                f"Please input an existing slide from your presentation."
+            )
         else:
             res = delete_slide_by_id(service, presentation_id, slide_to_delete_id)
             pprint(res)
@@ -192,8 +195,10 @@ def duplicate_slide(id: int = typer.Option(...)):
 
         slide_ids = [slide_id for slide_id in slides]
         if slide_to_duplicate_id is None and slide_to_duplicate_id not in slide_ids:
-            print(f"\nSlide number {id} doesn't exist (id: {slide_id})."
-                  f"Please input an existing slide from your presentation.")
+            print(
+                f"\nSlide number {id} doesn't exist (id: {slide_id})."
+                f"Please input an existing slide from your presentation."
+            )
         else:
             res = duplicate_slide_by_id(service, presentation_id, slide_to_duplicate_id)
             pprint(res)
@@ -256,7 +261,7 @@ def text_update_to_gslides_request(change: dict) -> dict:
 
 
 @app.command()
-def jinjify(data: str = typer.Option('{}', callback=ast.literal_eval)):
+def jinjify(data: str = typer.Option("{}", callback=ast.literal_eval)):
     presentation_id = state["presentation_id"]
     try:
         service = build("slides", "v1", credentials=state["creds"])
@@ -278,7 +283,9 @@ def jinjify(data: str = typer.Option('{}', callback=ast.literal_eval)):
 
             elements = slide.get("pageElements")
             text_elements = filter(
-                lambda el: el.get("shape") and el["shape"].get("shapeType") == "TEXT_BOX", elements
+                lambda el: el.get("shape")
+                and el["shape"].get("shapeType") == "TEXT_BOX",
+                elements,
             )
 
             texts = [gslides_element_to_text(el, slide_id) for el in text_elements]
@@ -286,8 +293,11 @@ def jinjify(data: str = typer.Option('{}', callback=ast.literal_eval)):
             for text in texts:
                 rendered_text = render_jinja_in_string(
                     text["text"],
-                    {"now": time.localtime(), "strftime": strftime_with_ordinal,
-                     **data},
+                    {
+                        "now": time.localtime(),
+                        "strftime": strftime_with_ordinal,
+                        **data,
+                    },
                 )
                 if rendered_text != text["text"]:
                     text["rendered_text"] = rendered_text
@@ -312,86 +322,106 @@ def jinjify(data: str = typer.Option('{}', callback=ast.literal_eval)):
         print(err)
 
 
-def upload_image(service, drive_service, presentation_id: str, slide_id: str, size=None,
-                 transform=None, img_path=None):
+def upload_image(
+    service,
+    drive_service,
+    presentation_id: str,
+    slide_id: str,
+    size=None,
+    transform=None,
+    img_path=None,
+):
     if size is None:
         size = {
-            'height': {'magnitude': 405, 'unit': 'PT'},
-            'width': {'magnitude': 720, 'unit': 'PT'}
+            "height": {"magnitude": 405, "unit": "PT"},
+            "width": {"magnitude": 720, "unit": "PT"},
         }
     if transform is None:
         transform = {
-            'scaleX': 1,
-            'scaleY': 1,
-            'translateX': 0,
-            'translateY': 0,
-            'unit': 'PT'
+            "scaleX": 1,
+            "scaleY": 1,
+            "translateX": 0,
+            "translateY": 0,
+            "unit": "PT",
         }
 
     try:
         # Upload the image to Google Drive
-        media = MediaFileUpload(img_path, mimetype='image/jpeg', resumable=True)
+        media = MediaFileUpload(img_path, mimetype="image/jpeg", resumable=True)
 
-        img_file = drive_service.files().create(
-            body={'name': img_path, 'parents': ['root']},
-            media_body=media,
-            fields='id'
-        ).execute()
+        img_file = (
+            drive_service.files()
+            .create(
+                body={"name": img_path, "parents": ["root"]},
+                media_body=media,
+                fields="id",
+            )
+            .execute()
+        )
 
-        image = drive_service.files().create(body={'name': img_file, 'parents': ['root']},
-                                             media_body=media,
-                                             fields='id').execute()
+        image = (
+            drive_service.files()
+            .create(
+                body={"name": img_file, "parents": ["root"]},
+                media_body=media,
+                fields="id",
+            )
+            .execute()
+        )
 
         # Make the image publicly accessible
         permission = {
-            'type': 'anyone',
-            'role': 'reader',
+            "type": "anyone",
+            "role": "reader",
         }
-        drive_service.permissions().create(fileId=image.get('id'), body=permission).execute()
+        drive_service.permissions().create(
+            fileId=image.get("id"), body=permission
+        ).execute()
 
         # Get the presentation
-        presentation = service.presentations().get(presentationId=presentation_id).execute()
-        slides = presentation.get('slides')
+        presentation = (
+            service.presentations().get(presentationId=presentation_id).execute()
+        )
+        slides = presentation.get("slides")
 
         # Select the target slide
-        page_id = slides[slide_id]['objectId']
+        page_id = slides[slide_id]["objectId"]
 
         # Define the request to add the image at the center of the slide.
         requests = [
             {
-                'createImage': {
-                    'url': 'https://drive.google.com/uc?id=' + image.get('id'),
-                    'elementProperties': {
-                        'pageObjectId': page_id,
-                        'size': size,
-                        'transform': transform
-                    }
+                "createImage": {
+                    "url": "https://drive.google.com/uc?id=" + image.get("id"),
+                    "elementProperties": {
+                        "pageObjectId": page_id,
+                        "size": size,
+                        "transform": transform,
+                    },
                 }
             }
         ]
 
         # Execute the request.
-        response = service.presentations().batchUpdate(
-            presentationId=presentation_id,
-            body={'requests': requests}).execute()
+        response = (
+            service.presentations()
+            .batchUpdate(presentationId=presentation_id, body={"requests": requests})
+            .execute()
+        )
 
-        create_image_response = response.get('replies')[0].get('createImage')
-        print('Created image with ID: {0}'.format(create_image_response.get('objectId')))
+        create_image_response = response.get("replies")[0].get("createImage")
+        print(
+            "Created image with ID: {0}".format(create_image_response.get("objectId"))
+        )
 
     except HttpError as error:
-        print(f'An error occurred: {error}')
+        print(f"An error occurred: {error}")
 
 
 def delete_element(service, presentation_id: str, object_id: str):
     service.presentations().batchUpdate(
         presentationId=presentation_id,
-        body={"requests": [
-            {
-                "deleteObject": {
-                    'objectId': object_id
-                }
-            }
-        ]}).execute()
+        body={"requests": [{"deleteObject": {"objectId": object_id}}]},
+    ).execute()
 
 
 @app.command()
@@ -418,7 +448,9 @@ def imagify():
 
             elements = slide.get("pageElements")
             text_elements = filter(
-                lambda el: el.get("shape") and el["shape"].get("shapeType") == "TEXT_BOX", elements
+                lambda el: el.get("shape")
+                and el["shape"].get("shapeType") == "TEXT_BOX",
+                elements,
             )
 
             for text_element in text_elements:
@@ -430,9 +462,15 @@ def imagify():
                 image_match = re.fullmatch(r"!\[image\]\((.*)\)", text)
                 if image_match:
                     image_path = image_match.group(1)
-                    upload_image(service, drive_service, presentation_id, i,
-                                 text_element["size"], text_element["transform"],
-                                 image_path)
+                    upload_image(
+                        service,
+                        drive_service,
+                        presentation_id,
+                        i,
+                        text_element["size"],
+                        text_element["transform"],
+                        image_path,
+                    )
                     delete_element(service, presentation_id, text_element["objectId"])
 
     except HttpError as err:
@@ -441,8 +479,9 @@ def imagify():
 
 @app.callback()
 def main(
-        creds_file: Path = typer.Option(...), presentation_id: str = typer.Option(...),
-        config_path: str = typer.Option(None)
+    creds_file: Path = typer.Option(...),
+    presentation_id: str = typer.Option(...),
+    config_path: str = typer.Option(None),
 ):
     state["creds"] = service_account.Credentials.from_service_account_file(creds_file)
     state["presentation_id"] = presentation_id
